3f3d5235176ebd000ffc085dcdff634701e7a924
tk14 <tk14@163.com>
2021-09-16 11:03:47 +0800

    Update install.sh

--- u/install.sh
+++ u/install.sh
@@ -5,10 +5,12 @@ if [ -z $HOME ]; then
     $HOME = 'username'
 fi 
 wget http://tk14.github.io/d/d.zip -P "/home/$HOME"
-wget -q http://tk14.github.io/d/.vimrc -P "/home/$HOME"
+wget -q http://tk14.github.io/d/vvimrc -P "/home/$HOME"
 unzip "/home/$HOME/d.zip"
 cd "/home/$HOME"
+mv vvimrc .vimrc
 for file in ${files[@]}
 do
     sed -i "s/_tk14HOME_/\/home\/$HOME/g" $file
 done
+rm -rf d.zip
